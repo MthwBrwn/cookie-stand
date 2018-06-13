28a788b9162976891088c2d93d0fdf4fd8a0a914
mthwbrwn <mthwbrwn@gmail.com>
2018-06-12 22:01:17 -0700

    not able to make the lists work

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,53 @@
 'use strict';
-
+console.log('hello there!');
 // this is my special '42' note!!
+
+
+// global variables
+var hours = ['6am ','7am','8am','9am','10am','11am','12am','13am','14am','15am','16am','17am','18am','19am','20am'];
+//array storing all cookie values
+var tableEl = this.document.getElementById('first' );
+
+function Store (location, min, max, avg){
+  this.location=location;
+  this.min = min;
+  this.max = max;
+  this.avg = avg;
+  this.cookieHours=0;
+  this.renderCookieRow();
+}
+
+Store.prototype.randomCookieValue = function () {
+  
+  return Math.round((Math.random() * (this.max - this.min) + this.min) * this.avg);
+};
+
+Store.prototype.renderCookieRow= function() {
+  // Locate table
+  
+  // possible for loop for this =
+  
+
+  var trEl = this.document.createElement('tr');
+  
+  for(var i = 0; i < hours.length; i++) {
+    //  create  elements
+    
+    var tdEl = this.document.createElement('td');
+    // attach content
+    
+    tdEl.textContent = this.randomCookieValue + ' cookies';
+    
+    // 3. append to the DOM
+    // parentElement.appendChild(childElement);
+    trEl.appendChild(tdEl);
+    //append data to
+  }
+  tableEl.appendChild(trEl);
+};
+
+var firstAndPike = new Store ( 23,65,6.3);
+
 
 // Location        | Min / Cust | Max / Cust | Avg Cookie / Sale
 // ----------------|------------|------------|-------------------
@@ -9,46 +56,3 @@
 // Seattle Center     |      11    |     38     |        3.7
 // Capitol Hill |      20    |     38     |        2.3
 // Alki            |      2     |     16     |        4.6
-
-// global variables 
-var hours= ['6am ','7am','8am','9am','10am','11am','12am','13am','14am','15am','16am','17am','18am','19am','20am'];
-//array storing all cookie values
-var cookieHours =0;
-/// 1st object creating PIKE store
-
- function Store (location, min, max, avg){
-  this.location=location;
-  this.min = min;
-  this.max = max;
-  this.avg = avg;
-  this.cookieHours=cookieHours;
-
-
-  this.numCookiesArr = function () {
-
-    return Math.round((Math.random() * (this.max - this.min) + this.min) * this.avg);
-  },
-
-  Store.prototype.renderCookieRow= function() {
-  // Locate table
-    var tableEl = this.document.getElementById('first' );
-    // possible for loop for this =
-    var trEl = this.document.createElement('tr');
-    
-    for(var i = 0; i < this.hours.length; i++) {
-      //  create  elements
-      
-      var tdEl = this.document.createElement('td');
-      
-      // attach content
-      
-      tdEl.textContent =  numCookiesArr + ' cookies';
-      
-      // 3. append to the DOM
-      // parentElement.appendChild(childElement);
-      tdEl.appendChild('tr');
-    };
-
-  },
- }
-
