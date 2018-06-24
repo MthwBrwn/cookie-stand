aac3e42643ab86cd0868ac95583bc2f9b1bd2925
mthwbrwn <mthwbrwn@gmail.com>
2018-06-24 09:39:48 -0700

    moving ul to inside object literal - 2nd object created

--- JS/app.js
+++ JS/app.js
@@ -13,17 +13,18 @@
 
 // global variables 
 var hours= ['6am ','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
+// get element from sales page - will be global 
+
+var ulPosition2 = document.getElementById('second');
+var ulPosition3 = document.getElementById('third');
+var ulPosition4 = document.getElementById('fourth');
+var ulPosition5 = document.getElementById('fifth');
 
 /// 1st object creating PIKE store
-
-      
-var ulPosition = document.getElementById('first');
-
 var pikeStore = {
   minCustomer: 23,
   maxCustomer: 65,
   avgCookiePerCust : 6.3,
-  cookiesPerHour : [],
   // returns random number between min max
   randomCookiesPerHour: function () {
     var rawRandomNum= ((Math.random() * (this.maxCustomer - this.minCustomer)) + this.minCustomer); 
@@ -31,7 +32,7 @@ var pikeStore = {
   },
   // applies calcdata to chart 
   renderCookiePike: function () {
-    
+    var ulPosition = document.getElementById('first');
     for (var i=0; i < hours.length; i++) { 
       //create li 
       var newElem = document.createElement('li');
@@ -45,7 +46,31 @@ var pikeStore = {
  
 pikeStore.renderCookiePike();
 
-
+// SeaTac Airport  |      3     |     24     |        1.2
+var seaTacStore = {
+  minCustomer: 3,
+  maxCustomer: 24,
+  avgCookiePerCust : 1.2,
+  
+  randomCookiesPerHour: function () {
+    var rawRandomNum= ((Math.random() * (this.maxCustomer - this.minCustomer)) + this.minCustomer); 
+    return Math.floor(rawRandomNum* this.avgCookiePerCust);
+  },
+   
+  renderCookiePike: function () {
+    var ulPosition = document.getElementById('second');
+    for (var i=0; i < hours.length; i++) { 
+      
+      var newElem = document.createElement('li');
+     
+      newElem.textContent= hours[i]+ ': '+ this.randomCookiesPerHour() + ' cookies' ;
+           
+      ulPosition.appendChild(newElem);
+    }
+  }
+};
+ 
+seaTacStore.renderCookiePike();
 
 
 
